1a5e051f84e84118fa4f314880f75c9977e5c462
Charlo <charlesoliviersabourin@gmail.com>
2026-01-04 10:42:14 -0500

    update-site

--- public/navbar.js
+++ public/navbar.js
@@ -55,3 +55,11 @@ document.addEventListener("DOMContentLoaded", async () => {
 });
 
 
+function myFunction() {
+  var x = document.getElementById("Links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
